b64ac713c66574d2f0c755af4ff153af288cdb22
Jeffrey <matheusunitt@gmail.com>
2024-01-06 11:00:32 -0300

    Adiciona componente Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer>
+      <span className="text-center w-full inline-block text-lg sm:text-xl">
+        feito por{" "}
+        <a
+          href="https://jeffreyalvr.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Clique para abrir o meu GitHub"
+          className="text-blue-600 underline hover:opacity-80"
+        >
+          @jeffreyalvr
+        </a>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
