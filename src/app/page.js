048d9643861413caa11f35ea11303091eb588eb3
Jeffrey <matheusunitt@gmail.com>
2024-01-06 09:17:19 -0300

    Adiciona funcionalidade de alterar o estado do todo

--- src/app/page.js
+++ src/app/page.js
@@ -10,12 +10,17 @@ const Home = async () => {
 
   const todos = await getTodos();
 
+  const toggleTodo = async (id, completed) => {
+    "use server";
+    await prisma.todo.update({ where: { id }, data: { completed } });
+  };
+
   return (
     <main className="w-[500px] h-fit p-6 rounded-lg border-2 mx-auto my-24">
       <h1 className="text-gray-500 text-3xl font-bold">Next JS Todo List</h1>
       <div className="my-8 border-t-2 border-b-2 py-4">
         {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </div>
       <Link
--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,7 +1,15 @@
-const TodoItem = ({ id, text, completed }) => {
+"use client";
+
+const TodoItem = ({ id, text, completed, toggleTodo }) => {
   return (
     <div className="flex flex-row gap-3" key={id}>
-      <input id={id} type="checkbox" className="peer cursor-pointer" />
+      <input
+        id={id}
+        type="checkbox"
+        className="peer cursor-pointer"
+        defaultChecked={completed}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
       <label
         htmlFor={id}
         className="peer-checked:line-through peer-checked:text-slate-500 cursor-pointer"
