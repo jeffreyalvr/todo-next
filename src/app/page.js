2618202dcc3fcd11ab8bcd8878939ce35fc06973
Jeffrey <matheusunitt@gmail.com>
2024-01-06 07:51:49 -0300

    Remove objeto placeholder; Reorganiza input dos todos

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,27 @@
 import Link from "next/link";
 
-const Home = () => {
-  const todos = [
-    { id: 0, text: "Estudar desenvolvimento web", completed: false },
-    { id: 1, text: "Estudar francês", completed: false },
-    { id: 2, text: "Fazer o workout", completed: false },
-    { id: 3, text: "Meditar", completed: false },
-  ];
+import { prisma } from "@/db";
+
+const Home = async () => {
+  const getTodos = () => {
+    return prisma.todo.findMany();
+  };
+
+  const todos = await getTodos();
 
   return (
     <main className="w-[500px] h-fit p-6 rounded-lg border-2 mx-auto my-24">
-      <h1 className="text-gray-500 text-3xl font-bold">Todos</h1>
+      <h1 className="text-gray-500 text-3xl font-bold">Next JS Todo List</h1>
       <div className="my-8 border-t-2 border-b-2 py-4">
         {todos.map((todo) => (
-          <div className="flex flex-row gap-4" key={todo.id}>
-            <input type="checkbox" />
-            <span>{todo.text}</span>
+          <div className="flex flex-row gap-3" key={todo.id}>
+            <input id={todo.id} type="checkbox" className="peer" />
+            <label
+              htmlFor={todo.id}
+              className="cursor-pointer peer-checked:line-through"
+            >
+              {todo.text}
+            </label>
           </div>
         ))}
       </div>
