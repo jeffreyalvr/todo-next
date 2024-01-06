6bbe714d7e7cd45045ee9d6bfdbc96e9606772bd
Jeffrey <matheusunitt@gmail.com>
2024-01-06 11:44:30 -0300

    Adiciona revalidatePath para lidar com o cache; Reorganiza o layout e código gerais; Adiciona o componente Footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,12 @@
 import Link from "next/link";
+import { revalidatePath } from "next/cache";
 
 import { prisma } from "@/db";
 import TodoItem from "@/components/TodoItem";
-import { redirect } from "next/navigation";
+
+import Footer from "../components/Footer";
 
 const Home = async () => {
-  const getTodos = () => {
-    return prisma.todo.findMany();
-  };
-
-  const todos = await getTodos();
-
   const toggleTodo = async (id, completed) => {
     "use server";
     await prisma.todo.update({ where: { id }, data: { completed } });
@@ -19,28 +15,41 @@ const Home = async () => {
   const handleExcluir = async (id) => {
     "use server";
     await prisma.todo.delete({ where: { id } });
-    redirect("/");
+    revalidatePath("/");
   };
 
+  const getTodos = async () => {
+    "use server";
+    return prisma.todo.findMany();
+  };
+
+  const todos = await getTodos();
+
   return (
-    <main className="w-[500px] h-fit p-6 rounded-lg border-2 mx-auto my-24">
-      <h1 className="text-gray-500 text-3xl font-bold">Next JS Todo List</h1>
-      <div className="my-8 border-t-2 border-b-2 py-4">
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            {...todo}
-            toggleTodo={toggleTodo}
-            handleExcluir={handleExcluir}
-          />
-        ))}
+    <main className="flex flex-col gap-8 w-[500px] h-fit mx-auto my-24">
+      <div className="w-full h-fit p-6 rounded-lg border-2">
+        <h1 className="flex gap-3 items-center text-gray-500 text-3xl font-bold">
+          Next JS Todo List
+          <span className="text-blue-600">({todos.length})</span>
+        </h1>
+        <div className="my-8 border-t-2 border-b-2 py-4">
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              toggleTodo={toggleTodo}
+              handleExcluir={handleExcluir}
+            />
+          ))}
+        </div>
+        <Link
+          href="/adicionar"
+          className="rounded-lg px-4 py-2 text-white bg-black hover:opacity-80"
+        >
+          Nova meta
+        </Link>
       </div>
-      <Link
-        href="/adicionar"
-        className="rounded-lg px-4 py-2 text-white bg-black hover:opacity-80"
-      >
-        Nova meta
-      </Link>
+      <Footer />
     </main>
   );
 };
